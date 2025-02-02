2bbc80d43c3ae9824966925b58515a536322b354
Nikolay Gendin <gendin786@gmail.com>
2025-02-02 15:28:22 +0300

    add new module  save_to_file.py in src

---
+++ src/save_to_file.py
@@ -0,0 +1,19 @@
+import json
+import logging
+
+logger = logging.getLogger("save_to_file")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/save_to_file.log")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+
+
+def save_to_file(data: list, file_path: str) -> None:
+    """Сохранение данных по указанному пути"""
+    try:
+        logger.info(f"Записываем данные в файл {file_path}")
+        with open(file_path, "w") as data_file:
+            json.dump(data, data_file, indent=4)
+    except Exception as e:
+        logger.error(f"Произошла ошибка: {e}")
